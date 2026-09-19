6770d1d91cb60fae6b2baa592496d39929cf8fab
SonyaKamaldinova <kamaldinovasona@gmail.com>
2026-09-19 17:52:55 +0300

    update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -87,4 +87,15 @@ public class Matrix {
         }
         return result;
     }
+
+    public Matrix transpose(){
+        Matrix result = new Matrix(cols, rows);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++){
+                result.set(j, i, data[i][j]);
+            }
+        }
+        return result;
+    }
 }
+
