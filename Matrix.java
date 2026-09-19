be50fd3f17f89e115d0e299d40d347d01794309e
SonyaKamaldinova <kamaldinovasona@gmail.com>
2026-09-19 16:24:11 +0300

    update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,28 @@
 public class Matrix {
+
     private final int rows;
     private final int cols;
+    private Complex[][] data;
+
+    public Matrix(int rows, int cols){
+        if (rows <= 0 || cols <= 0){
+            throw new IllegalArgumentException("Not the correct size");
+        }
+
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new Complex[rows][cols];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public Complex get(int row, int col){
+        return data[row][col];
+    }
 }
